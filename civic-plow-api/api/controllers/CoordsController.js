bc233ecffe425b4e3baace753b57a0129be6d06e
Peter B Smith <peter@upstate.agency>
2018-02-24 18:49:27 -0500

    feat: find closest 20 locations to coords

--- civic-plow-api/api/controllers/CoordsController.js
+++ civic-plow-api/api/controllers/CoordsController.js
@@ -6,8 +6,34 @@
  */
 
 module.exports = {
-	closest: function(req, res) {
-    const lookup = req.body;
+  closest: function (req, res) {
+    const lat = req.body.latitude;
+    const long = req.body.longitude;
+    const query = `SELECT latitude, longitude, address, date_fixed, truck_name,
+      (DEGREES(
+        ACOS(
+          SIN(RADIANS(?)) * SIN(RADIANS(latitude)) +
+          COS(RADIANS(?)) * COS(RADIANS(latitude)) *
+          COS(RADIANS(? - longitude))
+        )
+      ) * 60 * 1.1515)
+    AS distance FROM plow ORDER BY distance ASC LIMIT 20`;
+
+    Plow.query(query, [lat, lat, long], (err, plows) => {
+      if (err) {
+        console.dir(err);
+        res.status(400).send('err');
+      }
+      res.json(plows);
+    });
+  },
+  lastTen: function (req, res) {
+    Plow.find({}).limit(10).exec((err, plow) => {
+      if (err) {
+        res.status(400).send('err');
+      }
+      res.json(plow);
+    })
   }
 };
 
---
+++ civic-plow-api/api/models/Plow.js
@@ -0,0 +1,19 @@
+/**
+ * Plow.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  attributes: {
+    truck_name: 'string',
+    date_fixed: 'string',
+    address: 'string',
+    longitude: 'decimal',
+    latitude: 'decimal'
+  }
+};
+
